63c0345ae57713c3c77f33e4937a4484add535b4
Yangff <yangff1@gmail.com>
2016-05-06 20:09:41 +0800

    using v8 (*library is too large, ignored)

--- Projects/UCCU/src/ScriptCore.cpp
+++ Projects/UCCU/src/ScriptCore.cpp
@@ -1,35 +1,54 @@
 #include "ScriptCore.h"
+#include "uccuConfig.h"
 
 #include <stdlib.h>
 #include <string.h>
 #include <sstream>
 
-#if defined(_MSC_VER)
-// Sleep time for Windows is 1 ms while it's 1 ns for POSIX
-// Beware using this for your app. This is just to give a
-// basic idea on usage
-#include <windows.h>
-#else
-#include <unistd.h>
-#define Sleep(x) usleep(x)
-#endif
+#include <libplatform/libplatform.h>
+#include <v8.h>
 
-#include "jx.h"
+#pragma comment(lib,"icui18n.lib")
+#pragma comment(lib,"v8_libplatform.lib")
+#pragma comment(lib,"v8_libbase.lib")
+#pragma comment(lib,"v8_base_0.lib")
+#pragma comment(lib,"v8_base_1.lib")
+#pragma comment(lib,"v8_base_2.lib")
+#pragma comment(lib,"v8_base_3.lib")
+#pragma comment(lib,"v8_nosnapshot.lib")
+#pragma comment(lib,"icuuc.lib")
 
-void callback(JXResult *results, int argc) {
-	// do nothing
-}
+class ArrayBufferAllocator : public v8::ArrayBuffer::Allocator {
+public:
+	virtual void* Allocate(size_t length) {
+		void* data = AllocateUninitialized(length);
+		return data == NULL ? data : memset(data, 0, length);
+	}
+	virtual void* AllocateUninitialized(size_t length) { return malloc(length); }
+	virtual void Free(void* data, size_t) { free(data); }
+};
+
 
 ScriptCore::ScriptCore() {
-	JX_Initialize("RPGMV.exe", callback);
-	JX_InitializeNewEngine();
-	// JX_DefineMainFile("require(\"mvuccu\")");
-	JX_DefineMainFile("console.log('hello js!');");
+
 }
 
-void ScriptCore::RunScript()
-{
-	JX_StartEngine();
-	while (JX_LoopOnce() != 0) Sleep(1);
-	JX_StopEngine();
+void ScriptCore::RunScript() {
+	v8::V8::InitializeICU();
+	v8::V8::InitializeExternalStartupData("RPGMV.exe");
+	v8::Platform* platform = v8::platform::CreateDefaultPlatform();
+	v8::V8::InitializePlatform(platform);
+	v8::V8::Initialize();
+	// v8::V8::SetFlagsFromCommandLine(&argc, argv, true);
+	ArrayBufferAllocator array_buffer_allocator;
+	v8::Isolate::CreateParams create_params;
+	create_params.array_buffer_allocator = &array_buffer_allocator;
+	v8::Isolate* isolate = v8::Isolate::New(create_params);
+	{
+
+	}
+	isolate->Dispose();
+	v8::V8::Dispose();
+	v8::V8::ShutdownPlatform();
+	delete platform;
 }
