096369d97449f8735dbf28ba46ef421fee061aba
Yangff <yangff1@gmail.com>
2016-07-21 01:08:30 +0800

    ... this code is totally wrong ...

--- Projects/UCCU/src/JSCore.cpp
+++ Projects/UCCU/src/JSCore.cpp
@@ -40,6 +40,9 @@ namespace console {
 #include <QtCore/qstack.h>
 #include <QtCore/qdir.h>
 #include <QtCore/qfile>
+#include <QtCore/qjsondocument.h>
+#include <QtCore/qjsonobject.h>
+
 #include <v8pp/call_v8.hpp>
 
 namespace require {
@@ -49,10 +52,11 @@ namespace require {
 	};
 	
 	QMap<QString, v8::Persistent<v8::Value>> _internal;
-	QMap<QDir, v8::Persistent<v8::Value>> _external;
+	QMap<QFileInfo, v8::Persistent<v8::Value>> _external;
 
 	struct package_state {
 		QDir _current_dir;
+		QDir _package_dir;
 		QMap<QString, v8::Persistent<v8::Value>> _current_module;
 	};
 
@@ -72,8 +76,11 @@ namespace require {
 		v8::EscapableHandleScope handle_scope(isolate);
 		QFileInfo f(path);
 		if (f.exists()) {
+			if (_external.find(f) != _external.end())
+				return v8::Local<v8::Value>::New(v8::Isolate::GetCurrent(), _external[f]);
 			QDir _old_dir = package_stack.top()._current_dir;
 			package_stack.top()._current_dir = f.absoluteDir();
+			
 			QFile _f(path);
 			if (_f.open(QIODevice::ReadOnly)) {
 				QString s = _f.readAll();
@@ -81,21 +88,47 @@ namespace require {
 				v8::Local<v8::Object> module = v8::Object::New(isolate);
 				v8::Local<v8::Object> exports = v8::Object::New(isolate);
 				module->Set(v8::String::NewFromUtf8(isolate, "exports"), exports);
-				//v8::Persistent<v8::Object> module(isolate, );
-
+				
 				auto filename = v8::String::NewFromUtf8(isolate, f.fileName().toUtf8().data);
 				auto dirname = v8::String::NewFromUtf8(isolate, f.absoluteDir().absolutePath().toUtf8().data);
-
 				
 				v8::Local<v8::Context> context = v8::Context::New(isolate);
+				context->Global()->Set(v8::String::NewFromUtf8(isolate, "filename"), filename);
 				v8::Context::Scope context_scope(context);
 				v8::Local<v8::String> source =
 					v8::String::NewFromUtf8(isolate, s.toUtf8().data,
 					v8::NewStringType::kNormal).ToLocalChecked();
+
+				v8::TryCatch try_catch(isolate);
+
 				v8::Local<v8::Script> script = v8::Script::Compile(context, source).ToLocalChecked();
 				v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
-				auto f = v8::Local<v8::Function>::Cast(result);
-				v8pp::call_v8(isolate, f, isolate->GetCurrentContext()->Global(), exports, &require, module, )
+
+				if (try_catch.HasCaught()) {
+					if (try_catch.HasTerminated())
+						isolate->CancelTerminateExecution();
+					try_catch.ReThrow();
+
+					package_stack.top()._current_dir = _old_dir;
+					return handle_scope.Escape(v8::Null(isolate));
+				}
+
+				auto func = v8::Local<v8::Function>::Cast(result);
+				v8pp::call_v8(isolate, func, context->Global(), exports, &require, module, dirname, filename);
+				if (try_catch.HasCaught()) {
+					if (try_catch.HasTerminated())
+						isolate->CancelTerminateExecution();
+					try_catch.ReThrow();
+
+					package_stack.top()._current_dir = _old_dir;
+					return handle_scope.Escape(v8::Null(isolate));
+				}
+				
+				v8::Persistent<v8::Object> _module(isolate, exports);
+				_external[f] = _module;
+
+				package_stack.top()._current_dir = _old_dir;
+				return handle_scope.Escape(exports);
 			}
 
 		} else {
@@ -107,17 +140,49 @@ namespace require {
 	/* run package
 	   push package_stack  */
 	v8::Handle<v8::Value> load(const char * name) {
-		auto &s = package_stack.top();
-		QDir path(s._current_dir);
+		auto isolate = v8::Isolate::GetCurrent();
+		auto s = package_stack.top();
+
+		if (s._current_module.find(name) != s._current_module.end())
+			return v8::Handle<v8::Value>::New(isolate, s._current_module[name]);
+
+		QDir path(s._package_dir);
 		if (path.cd("node_modules"))
 			if (path.cd(name)) {
-				if (QFile(path.absoluteFilePath("package.json")).exists()) {
-					package_state S;
-					S._current_dir = path;
-					
+				QFile pkg(path.absoluteFilePath("package.json"));
+				QString main = "index.js";
+				if (pkg.exists() && pkg.open(QIODevice::ReadOnly)) {
+					QJsonParseError err;
+					QJsonDocument pkgd = QJsonDocument::fromJson(pkg.readAll(), &err);
+					if (err.error != QJsonParseError::NoError) {
+						LogManager::instance().log(QString("WARN (loading %1) Cannot parse (%2). \n %3 ").arg(name, path.absoluteFilePath("package.json"), err.errorString()));
+						// anyway, it will try load index.js
+					} else {
+						if (pkgd.object().find("main") != pkgd.object().end()) {
+							if (pkgd.object()["main"].isString())
+								main = pkgd.object()["main"].toString();
+						}
+					}
 				}
 				
+				QFileInfo entrance(path.absoluteFilePath(main));
+				if (entrance.exists() && entrance.isReadable()) {
+					package_state S;
+
+					S._package_dir = path;
+
+					package_stack.push(S);
+					
+					v8::Handle<v8::Value> r = run(entrance.absoluteFilePath);
+
+					package_stack.pop();
+
+					return r;
+				}
 			}
+		isolate->ThrowException(
+			v8::String::NewFromUtf8(isolate, QString("Cannot find package (%1).").arg(name).toUtf8().data)
+		);
 		return v8::Null(v8::Isolate::GetCurrent());
 	}
 
